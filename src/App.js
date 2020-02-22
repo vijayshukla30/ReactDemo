294cc162ba17448033dff5eed76b994a7c30cd85
Vijay Shukla <vshukla684@gmail.com>
2020-02-22 12:16:16 +0530

    Create Stores

--- src/App.js
+++ src/App.js
@@ -1,56 +1,11 @@
 import React, {Component, Fragment} from 'react';
 import {NavLink} from "react-router-dom";
 import Todo from "./Todo";
+import TodoStore from "./Store/TodoStore";
 
 class App extends Component {
   state = {
-    todos: [
-      {
-        id: 1,
-        title: "Introduction of Node and NPM",
-        status: false,
-      },
-      {
-        id: 2,
-        title: "Introduction to all frontend application and their usage",
-        status: false,
-      },
-      {
-        id: 3,
-        title: "Detail introduction of ReactJS (installation, setup)",
-        status: false,
-      },
-      {
-        id: 4,
-        title: "Discuss about package.json, index.html",
-        status: false,
-      },
-      {
-        id: 5,
-        title: "Basic React Component",
-        status: false,
-      },
-      {
-        id: 6,
-        title: "State and Styling",
-        status: false,
-      },
-      {
-        id: 7,
-        title: "props and PropTypes and iteration",
-        status: false,
-      },
-      {
-        id: 8,
-        title: "Function and this binding",
-        status: false,
-      },
-      {
-        id: 9,
-        title: "Other Stuff",
-        status: false,
-      }
-    ]
+    todos: TodoStore.getAllItem()
   };
 
   addTodo = (obj) => {
---
+++ src/Store/TodoStore.js
@@ -0,0 +1,62 @@
+import {EventEmitter} from 'events';
+
+class TodoStore extends EventEmitter {
+  constructor() {
+    super();
+    this.todos = [
+      {
+        id: 1,
+        title: "Introduction of Node and NPM",
+        status: false,
+      },
+      {
+        id: 2,
+        title: "Introduction to all frontend application and their usage",
+        status: false,
+      },
+      {
+        id: 3,
+        title: "Detail introduction of ReactJS (installation, setup)",
+        status: false,
+      },
+      {
+        id: 4,
+        title: "Discuss about package.json, index.html",
+        status: false,
+      },
+      {
+        id: 5,
+        title: "Basic React Component",
+        status: false,
+      },
+      {
+        id: 6,
+        title: "State and Styling",
+        status: false,
+      },
+      {
+        id: 7,
+        title: "props and PropTypes and iteration",
+        status: false,
+      },
+      {
+        id: 8,
+        title: "Function and this binding",
+        status: false,
+      },
+      {
+        id: 9,
+        title: "Other Stuff",
+        status: false,
+      }
+    ]
+  }
+
+  getAllItem() {
+    return this.todos;
+  }
+}
+
+const todoStore = new TodoStore;
+
+export default todoStore;

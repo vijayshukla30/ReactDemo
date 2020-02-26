e983f8a19602e1a985ddab9e9ececac716af2e94
Vijay Shukla <vshukla684@gmail.com>
2020-02-26 20:51:41 +0530

    Update User state according to the action

--- src/Store/user-reducer.js
+++ src/Store/user-reducer.js
@@ -1,4 +1,15 @@
 const userReducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'CHANGE_FIRST_NAME':
+      // Since we are mutating the state then we should always create a new object every time
+      // state.firstName = action.data;
+      state = {...state, firstName: action.data};
+      break;
+    case 'CHANGE_LAST_NAME':
+      // state.lastName = action.data;
+      state = {...state, lastName: action.data};
+      break;
+  }
   return state;
 };
 
